693fe10c554acf8a13abd007682b8fba5acf7a72
kimlai <kimlai@lrqdo.fr>
2019-02-08 16:49:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,126 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Fragment>
+        <MainNav />
+        <SalePage />
+      </Fragment>
+    );
+  }
+}
+
+class MainNav extends Component {
+  state = {
+    panelIsOpen: false
+  };
+
+  togglePanel = () => {
+    this.setState(prevState => ({
+      panelIsOpen: !prevState.panelIsOpen
+    }));
+  };
+
+  render() {
+    return (
+      <nav>
+        <div onClick={this.togglePanel} className="item">
+          Livraison
+        </div>
+        {this.state.panelIsOpen && <Panel close={this.togglePanel} />}
+      </nav>
+    );
+  }
+}
+
+class Panel extends Component {
+  state = {
+    selectedOption: 0,
+    pickups: [
+      { id: 0, name: "En Ruche", price: 0 },
+      { id: 1, name: "Livraison à domicile", price: 900 },
+      { id: 2, name: "Lulu dans ma ruche", price: 250 }
+    ]
+  };
+
+  selectOption = id => () => {
+    this.setState({
+      selectedOption: id
+    });
+  };
+
+  onSubmit = () => {
+    this.props.close();
+  };
+
+  render() {
+    return (
+      <div className="panel">
+        <h1>Retrait des produits</h1>
+        {this.state.pickups.map(pickup => (
+          <DeliveryOption
+            pickup={pickup}
+            isSelected={this.state.selectedOption === pickup.id}
+            onClick={this.selectOption(pickup.id)}
+          />
+        ))}
+        <button onClick={this.onSubmit}>Sauvegarder</button>
       </div>
     );
   }
 }
+
+const DeliveryOption = ({ pickup, isSelected, onClick }) => (
+  <div
+    key={pickup.id}
+    className={"option" + (isSelected ? " selected" : "")}
+    onClick={onClick}
+  >
+    {pickup.name + " (" + format(pickup.price) + ")"}
+  </div>
+);
+
+class SalePage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <SaleNav />
+        <Products />
+      </Fragment>
+    );
+  }
+}
+
+class SaleNav extends Component {
+  render() {
+    return (
+      <div className="saleNav">
+        <MiniBasket />
+      </div>
+    );
+  }
+}
+
+const Products = () => <div className="products" />;
+
+class MiniBasket extends Component {
+  state = {
+    price: 1245
+  };
+
+  render() {
+    return <div className="miniBasket">{format(this.state.price)}</div>;
+  }
+}
+
+const format = cents =>
+  cents === 0
+    ? "Gratuit"
+    : Math.floor(cents / 100) +
+      "," +
+      ((cents % 100) + "").padEnd(2, "0") +
+      " €";
 
 export default App;
